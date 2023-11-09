33dd91de26299ba72ed4222c2536db498a5d315b
Muzamil Adnan <muzamil.adnan@gildwaretechnologies.com>
2023-11-09 11:46:37 +0530

    validation added in new issue

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,41 +1,55 @@
 "use client"
-import { Button, TextField, TextFieldInput } from '@radix-ui/themes'
-import React from 'react'
+import { Button, Callout, Text, TextField, TextFieldInput } from '@radix-ui/themes'
+import React, { cache, useState } from 'react'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/ValidationSchema';
+import { z } from 'zod';
 
-interface IssueForm {
-    title: string,
-    description: string
-}
+type IssueForm = z.infer<typeof createIssueSchema>
 
 
 const NewIssuePage = () => {
-    const { register, control, handleSubmit } = useForm<IssueForm>();
+    const [error, setError] = useState("");
+    const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({ resolver: zodResolver(createIssueSchema) });
     const router = useRouter()
     return (
-        <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async(data) => {
-            
-            await axios.post('/api/issues', data)
-            router.push('/issues')
-        })}>
-            <TextField.Root>
-                <TextFieldInput placeholder='Title' {...register("title")} />
+        <div className='max-w-xl'>
+            {error && <Callout.Root className='mb-5 ' color='red'>
+                <Callout.Text>{error}</Callout.Text>
+            </Callout.Root>
+            }
+            <form className=' space-y-3' onSubmit={handleSubmit(async (data) => {
+                try {
+                    await axios.post('/api/issues', data)
+                    router.push('/issues')
+                }
+                catch (error) {
+                    setError("an unexpexted error occured")
+                }
 
-            </TextField.Root>
-            <Controller
-                name="description"
-                control={control}
-                render={({field}) => <SimpleMDE placeholder='description' {...field} />}
-            />
+            })}>
+                <TextField.Root>
+                    <TextFieldInput placeholder='Title' {...register("title")} />
+
+                </TextField.Root>
+                {errors.title && <Text color='red' as='p'>{errors.title.message}</Text>}
+                <Controller
+                    name="description"
+                    control={control}
+                    render={({ field }) => <SimpleMDE placeholder='description' {...field} />}
+                />
+                {errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
 
 
 
-            <Button>Submit issue</Button>
-        </form>
+                <Button>Submit issue</Button>
+            </form>
+        </div>
     )
 }
 
