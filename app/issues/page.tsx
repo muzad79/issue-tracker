2959e392b7edb802c652d3ab9e97bccc225875ec
Muzamil Adnan <muzamil.adnan@gildwaretechnologies.com>
2023-11-09 14:51:32 +0530

    added loading skeleton

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,42 @@
 import React from 'react'
-import { Button } from '@radix-ui/themes'
+import { Button, Table } from '@radix-ui/themes'
 import Link from 'next/link'
+import prisma from '@/prisma/client'
+import IssueStatusBadge from '../components/issueStatusBadge'
+import delay from 'delay'
+import IssueActions from './IssueActions'
 
-const Issue = () => {
+const Issue = async () => {
+  const issues = await prisma.issue.findMany();
+  await delay(2000);
   return (
-    
-    <Button><Link href='/issues/new'>new issue</Link></Button>
+    <div>
+      <IssueActions/>
+
+
+      <Table.Root variant='surface'>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>created</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>{issue.title}
+              <div className='block md:hidden'><IssueStatusBadge status={issue.status}/></div>
+              </Table.Cell>
+             
+              <Table.Cell className='hidden md:table-cell'><IssueStatusBadge status={issue.status}/></Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+
+      </Table.Root>
+    </div>
   )
 }
 
